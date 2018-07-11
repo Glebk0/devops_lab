5b20092c911a352c47339bcbec29f1202d8a4f2b
Glebk0 <student@EPBYMINW2472.minsk.epam.com>
2018-07-11 09:18:00 +0300

    fixed spaces

--- task4.py
+++ task4.py
@@ -8,7 +8,7 @@ def drawer(n, m):
     for i in range(1, n, 2):
         print((".|." * i).center(m, "-"))
     print("WELCOME".center(m, "-"))
-    for i in range(n-2, -1, -2):
+    for i in range(n - 2, -1, -2):
         print((".|." * i).center(m, "-"))
 
 

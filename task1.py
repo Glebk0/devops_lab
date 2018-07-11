6e07044848d65b3bac3797953d8120e73cd409e3
Glebk0 <glebko123@gmail.com>
2018-07-11 09:31:37 +0300

    minor edits

--- task1.py
+++ task1.py
@@ -3,7 +3,7 @@ def main():
     for _ in range(int(input("Enter number of students: "))):
         name, * \
             marks = input(
-                "Enter student name and marks separated by space: ").split()
+                "Enter student name and marks separated by space like 'Ann 22 55 79': ").split()
         avg[name] = [float(i) for i in marks]
     marks = avg[input("Enter student name whose average mark is needed: ")]
     print("Average mark is", "%.2f" % (sum(marks) / len(marks)))

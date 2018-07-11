eb22c84305e9c2ee38be54101a8cbe357a296400
Glebk0 <glebko123@gmail.com>
2018-07-11 17:48:30 +0300

    homework3 completed

---
+++ main.py
@@ -0,0 +1,30 @@
+import yaml
+import datetime
+import time
+import module
+
+with open('config.yaml', 'r') as config:
+    config_file = yaml.load(config)
+
+interval = config_file['interval']
+data_format = config_file["output"]
+iterator = 0
+
+
+def main():
+    time_format = datetime.datetime.fromtimestamp(time.time()).strftime('%d.%m.%Y %H:%M:%S')
+
+    logging = open('log.txt', "a+")
+    logging.write('\nSNAPSHOT {0}: {1}\n{2}\n'.format(
+        iterator,
+        time_format,
+        module.get_system_info(data_format)))
+
+    logging.close()
+
+
+if __name__ == '__main__':
+    while True:
+        main()
+        iterator +=1
+        time.sleep(interval)
---
+++ module.py
@@ -0,0 +1,50 @@
+import psutil
+import json
+
+
+def cpu_load():
+    cpu_load_var = psutil.cpu_percent(interval=5, percpu=False)
+    return cpu_load_var
+
+
+def virt_mem_status():
+    virt_mem_var = psutil.virtual_memory()
+    return virt_mem_var
+
+
+def swp_mem_status():
+    swp_mem_var = psutil.swap_memory()
+    return swp_mem_var
+
+
+def io():
+    io_count_var = psutil.disk_io_counters()
+    return io_count_var
+
+
+def net_stat():
+    net_stat_var = psutil.net_if_stats()
+    return net_stat_var
+
+
+def get_system_info(data_format):
+
+    if data_format == 'json':
+        data = dict()
+        data['cpu_load'] = cpu_load()
+        data['virt_mem_status'] = virt_mem_status()
+        data['swp_mem_status'] = swp_mem_status()
+        data['disk_io'] = io()
+        data['net_stat'] = net_stat()
+        return json.dumps(data)
+    else:
+        return ('CPU load                 : {0}%\n'
+                'Memory statistics        : {1}%\n'
+                'Virtual memory statistics: {2}%\n'
+                'Disk IO statistics       : {3}%\n'
+                'Network statistics       : {4}%\n'
+                .format(cpu_load(),
+                        virt_mem_status(),
+                        swp_mem_status(),
+                        io(),
+                        net_stat()))
